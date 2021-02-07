03be648cd932abf51e15fe6cf738c8314959aa92
RY Yuvraj <inventive8983@gmail.com>
2021-02-07 14:17:41 +0530

    final

--- admin/src/constants/ApiConstant.js
+++ admin/src/constants/ApiConstant.js
@@ -1,2 +1,4 @@
-export const API_BASE_URL = 'http://localhost:4000/api'
-export const BASE_URL = 'http://localhost:4000/'
+// export const API_BASE_URL = 'http://localhost:4000/api'
+export const API_BASE_URL = 'https://rhombusedu.herokuapp.com/api'
+// export const BASE_URL = 'http://localhost:4000/'
+export const BASE_URL = 'https://rhombusedu.herokuapp.com/'
